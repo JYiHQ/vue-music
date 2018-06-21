5b9871e1b8dbffa4f1a16c2d8375e93e3f5bf58a
cherryjin <1484133185@qq.com>
2018-06-21 17:56:48 +0800

    页面修改

--- src/components/list/list.vue
+++ src/components/list/list.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="wrapper">
-    <div class="row" v-for="(item, index) in listData" :key="item.id">
+    <div class="row" v-for="(item, index) in songList" :key="item.id">
       <div>{{index}}</div>
       <div></div>
     </div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'VList',
+  computed: {
+    ...mapState({
+      songList: state => state.songsDetails.songList,
+    }),
+  },
 };
 </script>
 <style scoped lang="stylus">
