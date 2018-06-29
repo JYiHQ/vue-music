d96c0d503c749b3471c18d1afa900bc3e6883ee1
cherryjin <1484133185@qq.com>
2018-06-29 11:03:15 +0800

    歌手列表页面开发

--- src/pages/HotSingers/components/HotSingersList.vue
+++ src/pages/HotSingers/components/HotSingersList.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="wrapper">
     <div class="content">
+      <div class="item" v-for="item in singerList" :key="item.id">
+        <div class="imgWrapper">
+          <img class="img" :src="item.picUrl"/>
+        </div>
+        <div class="singer-name">
+          <span class="span">{{item.name}}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,8 +28,37 @@ export default {
   .wrapper
     width 100%
     position relative
-    top 39px
+    top 45px
     .content
       width 100%
       display flex
+      flex-direction column
+      .item
+        width 100%
+        height 70px
+        display flex
+        align-items flex-end
+        -webkit-box-sizing: border-box
+        -moz-box-sizing: border-box
+        box-sizing: border-box
+        .imgWrapper
+          flex 0 0 17%
+          height 65px
+          display flex
+          align-items flex-end
+          justify-content flex-end
+          .img
+            width 55px
+            height 55px
+            border-radius 3px
+        .singer-name
+          flex 0 0 79%
+          height 55px
+          margin-left 10px
+          display flex
+          align-items center
+          -webkit-box-sizing: border-box
+          -moz-box-sizing: border-box
+          box-sizing: border-box
+          border-bottom  1px solid #ebeaee
 </style>
--- src/pages/HotSingers/index.vue
+++ src/pages/HotSingers/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrapper">
     <v-header :headerText="headerText"></v-header>
+    <hot-singers-list></hot-singers-list>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex';
 import VHeader from '../../components/header/VHeader';
+import HotSingersList from './components/HotSingersList';
 
 export default {
   name: 'HotSingers',
   components: {
     VHeader,
+    HotSingersList,
   },
   computed: {
     ...mapState({
